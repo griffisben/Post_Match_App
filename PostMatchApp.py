7bc47998517d06096ffab1cb8314ce156b2e6e90
Ben Griffis <griffis.ben16@gmail.com>
2026-06-18 09:20:28 +0200

    Update PostMatchApp.py

--- PostMatchApp.py
+++ PostMatchApp.py
@@ -16,6 +16,8 @@ cxG = 1.53570624482222
 
 @st.cache_data(ttl=60*15)
 
+good_leagues = ['K League 1','K League 2','World Cup']
+
 
 def color_percentile(pc):
     rgb = cmap(norm(pc))
@@ -185,26 +187,47 @@ league_data['xGA per 1 xT Against'] = league_data['xGA']/league_data['xT Against
 team_data['Open Play xGA per 1 xT Against'] = team_data['Open Play xGA']/team_data['xT Against']
 league_data['Open Play xGA per 1 xT Against'] = league_data['Open Play xGA']/league_data['xT Against']
 
-available_vars = ['Possession',
-                  'xG','xGA','xGD',
-                  'Open Play xG','Open Play xGA','Open Play xGD',
-                  'Set Piece xG','Set Piece xGA','Set Piece xGD',
-                  'xG within 5s of Crosses','xGA within 5s of Crosses',
-                  'xG within 15s of Set Pieces','xGA within 15s of Set Pieces','xGD within 15s of Set Pieces',
-                  'npxG','npxGA','npxGD',
-                  'GD','GD-xGD',
-                  'xPts','Pts-xPts',
-                  'Goals','Goals Conceded',
-                  'Shots','Shots Faced','Field Tilt','Field Tilt - Possession','Avg Pass Height','Passes in Opposition Half','Passes into Box','xT','xT Against','xT Difference','Shots per 1 xT','Shots Faced per 1 xT Against',
-                  'xG per 1 xT','xGA per 1 xT Against',
-                  'Open Play xG per 1 xT','Open Play xGA per 1 xT Against',
-                  'PPDA','High Recoveries','High Recoveries Against','Crosses','Corners','Fouls',
-                 'Throw-Ins into the Box','On-Ball Pressure','On-Ball Pressure Share','Off-Ball Pressure','Off-Ball Pressure Share','Game Control','Game Control Share',
-                 ]
-
-rank_vars = ['xPts','Possession','Field Tilt','Goals','Goals Conceded','Open Play xG','Open Play xGA','xG within 5s of Crosses','xGA within 5s of Crosses','xG within 15s of Set Pieces','xGA within 15s of Set Pieces','xT Difference','Open Play xG per 1 xT','PPDA','High Recoveries','High Recoveries Against',]
-rank_tfs = [False,False,False,False,True,False,True,False,True,False,True,False,False,True,False,True]
-rank_tfs_inv = [True,True,True,True,False,True,False,True,False,True,False,True,True,False,True,False]
+if lgg in good_leagues:
+    available_vars = ['Possession',
+                      'xG','xGA','xGD',
+                      'Open Play xG','Open Play xGA','Open Play xGD',
+                      'Set Piece xG','Set Piece xGA','Set Piece xGD',
+                      'xG within 5s of Crosses','xGA within 5s of Crosses',
+                      'xG within 15s of Set Pieces','xGA within 15s of Set Pieces','xGD within 15s of Set Pieces',
+                      'npxG','npxGA','npxGD',
+                      'GD','GD-xGD',
+                      'xPts','Pts-xPts',
+                      'Goals','Goals Conceded',
+                      'Shots','Shots Faced','Field Tilt','Field Tilt - Possession','Avg Pass Height','Passes in Opposition Half','Passes into Box','xT','xT Against','xT Difference','Shots per 1 xT','Shots Faced per 1 xT Against',
+                      'xG per 1 xT','xGA per 1 xT Against',
+                      'Open Play xG per 1 xT','Open Play xGA per 1 xT Against',
+                      'PPDA','High Recoveries','High Recoveries Against','Crosses','Corners','Fouls',
+                     'Throw-Ins into the Box','On-Ball Pressure','On-Ball Pressure Share','Off-Ball Pressure','Off-Ball Pressure Share','Game Control','Game Control Share',
+                     ]
+    rank_vars = ['xPts','Possession','Field Tilt','Goals','Goals Conceded','Open Play xG','Open Play xGA','xG within 5s of Crosses','xGA within 5s of Crosses','xG within 15s of Set Pieces','xGA within 15s of Set Pieces','xT Difference','Open Play xG per 1 xT','PPDA','High Recoveries','High Recoveries Against',]
+    rank_tfs = [False,False,False,False,True,False,True,False,True,False,True,False,False,True,False,True]
+    rank_tfs_inv = [True,True,True,True,False,True,False,True,False,True,False,True,True,False,True,False]
+    
+else:
+    available_vars = ['Possession',
+                      'xG','xGA','xGD',
+                      'Open Play xG','Open Play xGA','Open Play xGD',
+                      'Set Piece xG','Set Piece xGA','Set Piece xGD',
+                      # 'xG within 5s of Crosses','xGA within 5s of Crosses',
+                      # 'xG within 15s of Set Pieces','xGA within 15s of Set Pieces','xGD within 15s of Set Pieces',
+                      'npxG','npxGA','npxGD',
+                      'GD','GD-xGD',
+                      'xPts','Pts-xPts',
+                      'Goals','Goals Conceded',
+                      'Shots','Shots Faced','Field Tilt','Field Tilt - Possession','Avg Pass Height','Passes in Opposition Half','Passes into Box','xT','xT Against','xT Difference','Shots per 1 xT','Shots Faced per 1 xT Against',
+                      'xG per 1 xT','xGA per 1 xT Against',
+                      'Open Play xG per 1 xT','Open Play xGA per 1 xT Against',
+                      'PPDA','High Recoveries','High Recoveries Against','Crosses','Corners','Fouls',
+                     'Throw-Ins into the Box','On-Ball Pressure','On-Ball Pressure Share','Off-Ball Pressure','Off-Ball Pressure Share','Game Control','Game Control Share',
+                     ]
+    rank_vars = ['xPts','Possession','Field Tilt','Goals','Goals Conceded','Open Play xG','Open Play xGA','xT Difference','Open Play xG per 1 xT','PPDA','High Recoveries','High Recoveries Against',]
+    rank_tfs = [False,False,False,False,True,False,True,False,False,True,False,True]
+    rank_tfs_inv = [True,True,True,True,False,True,False,True,True,False,True,False]
 
 
 team_data[available_vars] = team_data[available_vars].astype(float)
